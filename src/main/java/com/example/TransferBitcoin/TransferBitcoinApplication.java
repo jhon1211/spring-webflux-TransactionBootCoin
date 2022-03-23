60450dee99b71bf2c35bce43052d65a4062153ed
Jhon Ccamerccoa Perez <jccamerc@emeal.nttdata.com>
2022-03-23 12:12:24 -0500

    first commit

---
+++ src/main/java/com/example/TransferBitcoin/TransferBitcoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.TransferBitcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferBitcoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferBitcoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TransferBitcoin/TransferBitcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TransferBitcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferBitcoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
